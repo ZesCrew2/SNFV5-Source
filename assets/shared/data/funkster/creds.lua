9e8e6310305e01ee8df1e2d5d7bbfc346a31fc4f
Lily (ZesCrew2) <75373543+ZesCrew2@users.noreply.github.com>
2025-07-14 06:45:07 -0400

    funkster (story mode) now ends with cred.mp4
    hehehehehehe

---
+++ assets/shared/data/funkster/creds.lua
@@ -0,0 +1,9 @@
+local allowCountdown = false
+function onEndSong()
+    if isStoryMode and not seenCutscene then
+        startVideo('creds1')
+        seenCutscene = true
+        return Function_Stop
+    end
+    return Function_Continue
+end
