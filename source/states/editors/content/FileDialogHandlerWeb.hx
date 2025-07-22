fef07b85607bb01033a1266f2cbb5d9dfc057963
Jake Official ♪ <141687741+Jake-Official@users.noreply.github.com>
2025-07-22 21:54:40 +0100

    Create FileDialogHandlerWeb.hx

---
+++ source/states/editors/content/FileDialogHandlerWeb.hx
@@ -0,0 +1,9 @@
+package states.editors.content;
+
+class FileDialogHandler {
+    public function new() {}
+
+    public function openFile():Void {
+        trace("File dialogs are not supported on HTML5.");
+    }
+}
